b8af836496c4d30d0752752d677fb267930cefa3
Antti-Jussi Niku <anttijussi.niku@gmail.com>
2022-11-08 14:54:18 +0200

    Lisatty tiedon hakeminen serverilta components/Temperature

--- Client/src/components/temperature.js
+++ Client/src/components/temperature.js
@@ -1,7 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+const URL = "http://localhost:3001"
+
 function Temperature() {
+    const [tempTable, setTempTable] = useState([])
+
+    //Paikat naille voi muuttua myohemmin...
+    useEffect(() => {
+      try {
+        const response = axios.get(URL+"/datasets")
+        .then((response) => {
+          console.log(response.data.v1data[0].globalAnnual)
+          setTempTable(response.data.v1data[0].globalAnnual.map(item=>item))
+        });
+      } catch (error) {
+        console.log(error)
+      }
+    }, [])
+
+    //Temporary test below will be changed to chart later on...
     return (
-        <h1>Temperature</h1>
+      <table>
+      <h3>V1 Temperature Anomalies (this will be replaced with a chart...)</h3>
+      <tbody>
+          <tr>
+              <th>time</th>
+              <th>anomaly</th>
+          </tr>
+          {tempTable.map(p => (
+          <tr key={p}>
+              <td>{p.time}</td>
+              <td>{p.anomaly}</td>
+          </tr>
+          ))}
+      </tbody>
+    </table>
     )
 }
 
 export default Temperature;
+
