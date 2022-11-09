60fee4b1534c314a2e1d980dec7497efc9027084
Junnu Kyrö <junnukyro@outlook.com>
2022-11-09 19:25:00 +0200

    Chart testi

--- Client/src/components/temperature.js
+++ Client/src/components/temperature.js
@@ -1,42 +1,108 @@
 import React, { useState, useEffect } from 'react';
-import {Chart} from 'chart.js';
+import { Chart, registerables } from 'chart.js';
+import { Line } from 'react-chartjs-2';
 import axios from 'axios'
 const URL = "http://localhost:3001"
 
+Chart.register(...registerables);
+
 function Temperature() {
-    const [tempTable, setTempTable] = useState([])
+  let years = [];
+  let anomaly = [];
 
-    //Paikat naille voi muuttua myohemmin...
-    useEffect(() => {
-      try {
-        const response = axios.get(URL+"/datasets")
+  const [years2, setYears] = useState([]);
+  const [globalAnnualAnomaly, setGlobalAnnualAnomaly] = useState([]);
+  const [globalMonthlyAnomaly, setGlobalMonthlyAnomaly] = useState([]);
+  const [northernAnnualAnomaly, setNorthernAnnualAnomaly] = useState([]);
+  const [northernMonthlyAnomaly, setNorthernMonthlyAnomaly] = useState([]);
+
+
+  //Paikat naille voi muuttua myohemmin...
+  useEffect(() => {
+    try {
+      const response = axios.get(URL + "/datasets")
         .then((response) => {
-          console.log(response.data.v1data[0].globalAnnual)
-          setTempTable(response.data.v1data[0].globalAnnual.map(item=>item))
-        });
-      } catch (error) {
-        console.log(error)
-      }
-    }, [])
 
-    //Temporary test below will be changed to chart later on...
-    return (
-      <table>
+          console.log(response.data.v1data);
+          response.data.v1data[0].globalAnnual.forEach(element => {
+            years.push(element.time);
+            anomaly.push(element.anomaly);
+          });
+          setYears(years);
+          setGlobalAnnualAnomaly(anomaly);
+          
+          anomaly = [];
+          response.data.v1data[0].globalMonthly.forEach(element => {
+            anomaly.push(element.anomaly);
+          });
+          setGlobalMonthlyAnomaly(anomaly);
+
+          anomaly = [];
+          response.data.v1data[0].northernAnnual.forEach(element => {
+            anomaly.push(element.anomaly);
+          });
+          setNorthernAnnualAnomaly(anomaly);
+
+          anomaly = [];
+          response.data.v1data[0].northernMonthly.forEach(element => {
+            anomaly.push(element.anomaly);
+          });
+          setNorthernMonthlyAnomaly(anomaly);
+
+        });
+
+    } catch (error) {
+      console.log(error)
+    }
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Temperature Anomalies from 1850",
+      },
+    },
+  };
+
+
+  //Temporary test below will be changed to chart later on...
+  return (
+    <div>
       <h3>V1 Temperature Anomalies (this will be replaced with a chart...)</h3>
-      <tbody>
-          <tr>
-              <th>time</th>
-              <th>anomaly</th>
-          </tr>
-          {tempTable.map(p => (
-          <tr key={p}>
-              <td>{p.time}</td>
-              <td>{p.anomaly}</td>
-          </tr>
-          ))}
-      </tbody>
-    </table>
-    )
+      <Line
+      style={{backgroundColor: "white"}}
+        options={options}
+        data={{
+          labels: years2,
+          datasets: [
+            {
+              label: "Global Annual",
+              data: globalAnnualAnomaly,
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+            {
+              label: "Global Monthly",
+              data: globalMonthlyAnomaly,
+            },
+            {
+              label: "Northern Annual",
+              data: northernAnnualAnomaly,
+            },
+            {
+              label: "Northern Monthly",
+              data: northernMonthlyAnomaly,
+            }
+          ]
+        }}
+      />
+    </div>
+
+  )
 }
 
 export default Temperature;
