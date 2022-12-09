ea7249f62fabe8f54d860ff86560894a920c4583
Junnu Kyrö <junnukyro@outlook.com>
2022-12-09 12:08:12 +0200

    V7V10 toimii, History event data pitää lyhentää

--- Client/src/components/charts/v7.js
+++ Client/src/components/charts/v7.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Chart, registerables } from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import React, { useState, useEffect, useContext, useRef } from 'react';
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Chart } from 'react-chartjs-2';
 import Card from 'react-bootstrap/Card';
 import { DatasetsContext } from '../../App.js';
 
-Chart.register(...registerables);
+ChartJS.register(...registerables);
 
 function V7(props) {
 
@@ -12,26 +12,99 @@ function V7(props) {
 
     const [timeTemp1, setTimeTemp1] = useState([]);
     const [globalTemp1, setGlobalTemp1] = useState([]);
-    const [timeCarbon1, setTimeCarbon1] = useState([]);
     const [carbon1, setCarbon1] = useState([]);
+    const [humanEvolution1, setHumanEvolution] = useState([]);
 
     useEffect(() => {
         if (datasets.length !== 0) {
-            let timeCarbon = datasets.v7data[0].carbon_dioxide.map(x => x.time_kyr_bp);
             let carbon = datasets.v7data[0].carbon_dioxide.map(x => x.carbon_dioxide_ppm);
-            setTimeCarbon1(timeCarbon);
             setCarbon1(carbon);
 
             let timeTemp = datasets.v7data[0].gast_reconstruction.map(x => x.time_kyr_bp);
+            //multiple timeTemp data by 1000 to get the correct time and add - char to beginning of string
+            timeTemp = timeTemp.map(x => x * 1000);
+            timeTemp = timeTemp.map(x => "-" + x);
+
             let globalTemp = datasets.v7data[0].gast_reconstruction.map(x => x.changes_global_tempature_c);
             setTimeTemp1(timeTemp);
             setGlobalTemp1(globalTemp);
+
+            let humanEvolution = datasets.v10data;
+
+            //filters out years that are not between -2000000 and 0
+            humanEvolution = humanEvolution.filter(x => x.year >= -2000000 && x.year <= 0);
+
+            //we must select only important events from data!!!
+
+            setHumanEvolution(humanEvolution);
         }
 
     }, [datasets]);
 
+    const data = {
+        labels: timeTemp1,
+        datasets: [
+            {
+                type: 'line',
+                label: "Change in global temperature (ºC)",
+                data: globalTemp1,
+                borderColor: 'blue',
+                borderWidth: 2,
+                yAxisID: 'y1',
+                xAxisID: 'x1'
+
+            },
+            {
+                type: 'line',
+                label: "C02 measurements from the 800k year period",
+                data: carbon1,
+                borderColor: 'red',
+                borderWidth: 2,
+                yAxisID: 'y2',
+            },
+            {
+                type: 'bubble',
+                label: "History",
+                data: humanEvolution1.map((x) =>{
+                    return {
+                        x: x.year,
+                        y: 4,
+                        r: 5,
+                        description: x.description,
+                    }
+                }),
+                borderColor: 'orange',
+                borderWidth: 2,
+            }
+        ]
+    }
+
     const options = {
         plugins: {
+            tooltip: {
+                callbacks: {
+                    title: function (context) {
+                        var title = context[0].dataset.title;
+                        if(context[0].dataset.type === "bubble"){
+                            title = "Year: " + context[0].parsed.x;
+                        }
+                        else{
+                            title = "Year: " + context[0].label;
+                        }
+                        return title;
+                    },
+                    label: function (context) {
+                        var label = context.dataset.label;
+                        if(context.dataset.type === "bubble"){
+                            label = context.raw.description;
+                        }
+                        else{
+                            label = label + ": " + context.formattedValue;
+                        }
+                        return label;
+                    }
+                },
+            },
             legend: {
                 position: "top",
             },
@@ -39,9 +112,10 @@ function V7(props) {
                 display: true,
                 text: "Evolution of global temperature over the past two million years combined ",
             },
+            
         },
         interaction: {
-            mode: 'index',
+            mode: 'nearest',
             intersect: false,
         },
         elements: {
@@ -52,8 +126,6 @@ function V7(props) {
         scales: {
             y1: {
                 position: 'right',
-                min: -5,
-                max: 15,
                 title: {
                     display: true,
                     text: "Surface temperature change (ºC)",
@@ -67,8 +139,6 @@ function V7(props) {
             },
             y2: {
                 position: 'left',
-                min: 100,
-                max: 300,
                 title: {
                     display: true,
                     text: "C02 ppm",
@@ -82,11 +152,10 @@ function V7(props) {
 
             },
             x1: {
-                reverse: true,
                 type: "linear",
                 title: {
                     display: true,
-                    text: "Time (ka)",
+                    text: "Time, 2 million years ago to present",
                     color: 'black',
                     font: {
                         size: 12,
@@ -99,29 +168,6 @@ function V7(props) {
 
     };
 
-    const data = {
-        labels: timeTemp1,
-        datasets: [
-            {
-                label: "Change in global temperature (ºC)",
-                data: globalTemp1,
-                showLine: true,
-                borderColor: 'blue',
-                borderWidth: 2,
-                yAxisID: 'y1',
-                xAxisID: 'x1'
-
-            },
-            {
-                label: "C02 measurements from the 800k year period",
-                data: carbon1,
-                showLine: true,
-                borderColor: 'red',
-                borderWidth: 2,
-                yAxisID: 'y2',
-            },
-        ]
-    }
 
     return (
         <>
@@ -136,9 +182,10 @@ function V7(props) {
                         ? <p>{props.description}</p>
                         : null
                     }
-                    <div style={{ width: 'auto', height: 'auto'}}>
-                        <Line
-                            style={{ backgroundColor: "white" }}
+                    <div style={{ width: 'auto', height: 'auto' }}>
+
+                        <Chart
+                            type='line'
                             options={options}
                             data={data}
                         />
