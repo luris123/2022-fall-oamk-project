3dc212f6cd2912079da12f2f6ccca9d03dc9c2f5
Junnu Kyrö <junnukyro@outlook.com>
2022-12-12 17:10:51 +0200

    Muokkauksia

---
+++ Server/config/allowedOrigins.js
@@ -0,0 +1,4 @@
+//allowed origins
+const allowedOrigins = ['http://localhost:3000', 'https://group5-visualizationtool-wncw.onrender.com', 'https://group5-visualizationtool-wncw.onrender.com/'];
+
+module.exports = allowedOrigins;
